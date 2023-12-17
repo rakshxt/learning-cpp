2cd00bd1bf0849ea996dd89909c3a9618aac2a6b
rakshxt <rakshxt@gmail.com>
2023-12-18 01:59:09 +0530

    1

--- hw_basic/hw_pause_on_konsole.cpp
+++ hw_basic/hw_pause_on_konsole.cpp
@@ -7,7 +7,8 @@ int main(){
     std::cout << "Hello, World!";
 
     std::cin.clear(); // reset any error flags
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+    // ignore any characters in the input buffer until we find an enter character
     std::cin.get(); // get one more char from the user
     
     return 0;
