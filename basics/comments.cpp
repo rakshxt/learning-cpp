77d52d17f0d1571a2a194d18e186ccc49c541890
rakshxt <rakshxt@gmail.com>
2023-12-19 22:30:10 +0530

    xx

---
+++ basics/comments.cpp
@@ -0,0 +1,24 @@
+// commenting out the preprocessor statement because i dont really want to execute this program
+// #include <iostream> 
+
+int main(){
+    /*
+        Comments are basically "lines" in a source code that are always ignored by the compiler (like my whishes gets ignored by my fam)
+        The most general motion/use of comments is to make the programmer understand "what", "how" and "why" of the specific parts of the program
+
+        C++ offers two types of comments:
+                                        1. Single line comments(//)
+                                        2. Multi  line comments(/** /)
+
+    */
+
+   // this is a single line comment
+   // use this comment in the times when explaining why a statement is doing "something"
+   // like set x=0  (explain why we are setting x=0)
+
+   /*   
+        This is a multi line comment is basically used when we gotta comment out a large number of lines 
+        NOTE: multiline comments cannot be nested however we can use single line comments inside a multiline comment
+        // ex
+   */
+}
