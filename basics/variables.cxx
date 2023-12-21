00c3c82df58e70aa57197c08ae9e8f71b84bd864
rakshxt <rakshxt@gmail.com>
2023-12-21 23:24:47 +0530

    x

--- basics/variables.cxx
+++ basics/variables.cxx
@@ -8,14 +8,17 @@ int main(){
     int radius;
     radius=3;   // assignment (giving of value) to the variable is done with the help of assignment operator (=)
     std::cout << radius;  // prints 3
+
     /**
      * mechanism :: we defined a variable (int) named radius
      *              on the next line we gave a value(integer) 3 to the variable "radius"
      *              we then pass the variable to the std::cout function of the iostream library
      *              when we call the "radius" variable in std::cout, the program refers to the value stored in the memory location that was assigned to the variable
     */
+    
     radius=10;
     std::cout << radius;  // prints 10
+    
     /**
      * mechanism :: after firstly the radius(3) was printed we changed the value stored in the memory region of radius to 10
      *              this overwrites the previously stored value in the memory (i.e 3) with new assigned value (i.e. 10)
