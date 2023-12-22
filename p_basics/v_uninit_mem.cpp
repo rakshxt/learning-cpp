db3e4bd4b5c6c211726c484218839c3d40dc1383
rakshxt <rakshxt@gmail.com>
2023-12-22 17:24:30 +0530

    xx

---
+++ p_basics/v_uninit_mem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    // int m{}, n{}; // value initlialization doesnt work (as value initialization already gives the variable an initial value of 0)
+    int m, n;
+    std::cout << "garbage values : " << m << ", " << n << '\n';
+
+    return 0;
+}
