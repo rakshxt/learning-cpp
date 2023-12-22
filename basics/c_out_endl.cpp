70d27739243841ddf9a0de75e878cfeebdc2516e
rakshxt <rakshxt@gmail.com>
2023-12-22 16:21:27 +0530

    .

--- basics/c_out_endl.cpp
+++ basics/c_out_endl.cpp
@@ -5,7 +5,7 @@ int main(){
     // std::cout << "Hello, Rakshit" << std::endl;
     // std::cout << "Welcome to the world of C++" << std::endl;
     
-    int age {17};
+    int age {};
     std::cout << "name : " << "rakshxt" << std::endl;
     std::cout << "age : " << age << std::endl;
     
